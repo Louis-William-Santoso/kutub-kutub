e0eff12ed833842e6ddfd3a712235ef896ac7d4a
Philbert51 <joshplayz862@gmail.com>
2025-10-30 15:36:01 +0700

    Update querySoal.sql

--- querySoal/querySoal.sql
+++ querySoal/querySoal.sql
@@ -1,50 +1,82 @@
-# a. Tampilkan seluruh video yang terdaftar pada sistem dan urutkan video tersebut berdasarkan like terbanyak hingga tersedikit.
-select v.id_video, v.judul_video, u.username as uploader, v.likes
-from kutube_db.video v left join kutube_db.user u on v.id_uploader =u.id_user
-order by v.likes desc;
+-- a. Tampilkan seluruh video dan urutkan berdasarkan jumlah likes terbanyak hingga tersedikit
+SELECT V.id_video AS ID_VIDEO, V.likes AS likes 
+FROM video V 
+ORDER BY likes DESC;
 
-# b. Tampilkan apa saja jenjang usia yang tercatat dalam sistem dan jumlah pengguna pada masing-masing jenjang usia tersebut.
-select pendidikan, min(year(now())-year(tanggal_lahir)) as min_year, max(year(now())-year(tanggal_lahir)) as max_year
-from kutube_db.user 
-group by pendidikan ;
 
-# c. Tampilkan playlist yang dimiliki oleh pengguna dengan jenis penonton yang mendaftar pertama kali pada platform kutube.
-select id_playlist, (date(u.created_at)) as tanggalBuatAkun,u.username, play.judul_playlist
-from kutube_db.playlist play, kutube_db.user u 
-inner join kutube_db.user_has_playlist up
-on u.id_user=up.user_id_user
-group by play.judul_playlist
-having min(year(u.created_at))
-order by id_playlist asc limit 1;
+-- b. Tampilkan daftar umur pengguna dan jumlah pengguna pada masing-masing umur
+SELECT FLOOR(DATEDIFF(CURDATE(), user.tanggal_lahir) / 365) AS Age, 
+       COUNT(user.tanggal_lahir) AS amount 
+FROM user 
+GROUP BY Age 
+ORDER BY Age ASC;
 
-# d. Tampilkan pengguna dengan jumlah video ditonton terbanyak dan tampilkan dari yang paling sedikit ke paling banyak.
-select video.id_video, video.judul_video, video.views
-from kutube_db.video
-order by video.views asc;
 
-# e. Tampilkan siapa saja penyedia video yang telah memenuhi persyaratan untuk mendapatkan adsense.
-select username, user_type, is_verified
-from kutube_db.user
-where is_verified=1 and user_type='Creator';
+-- c. Tampilkan playlist yang dimiliki oleh pengguna dengan jenis penonton yang mendaftar pertama kali
+SELECT U.id_user AS ID, 
+       U.username AS Username, 
+       U.created_at AS `User Creation Date`, 
+       P.id_playlist AS ID_Playlist, 
+       P.judul_playlist, 
+       P.description 
+FROM user U 
+JOIN user_has_playlist UP ON UP.user_id_user = U.id_user 
+JOIN playlist P ON P.id_playlist = UP.playlist_id_playlist 
+WHERE U.user_type = 'Penonton' 
+ORDER BY U.created_at ASC, UP.playlist_id_playlist ASC 
+LIMIT 1;
 
-# f. (Lanjutan 4g) Tampilkan berapa banyak video iklan yang diperoleh oleh orang-orang pada soal 4g dalam bulan September 2025 – Oktober 2025.
-select v.judul_video, v.upload_time, ads.jumlah_iklan
-from kutube_db.video v inner join kutube_db.adsense ads
-on v.id_video=ads.id_video
-where upload_time<='2025-10-31' and upload_time>='2025-09-01';
 
-# g. Tampilkan top 5 viewer video sepanjang tahun 2025
-select video.judul_video, video.views
-from kutube_db.video
-where upload_time>=2025-01-01
-order by video.views desc limit 5;
+-- d. Tampilkan pengguna berdasarkan jumlah video yang telah ditonton, dari paling sedikit ke paling banyak
+SELECT U.id_user AS ID_USER, 
+       U.username AS Name, 
+       COUNT(W.id_video) AS Watched 
+FROM user U 
+JOIN watch_history W ON U.id_user = W.id_user 
+GROUP BY U.id_user 
+ORDER BY Watched ASC;
 
-# h. Tampilkan top 5 kategori video yang paling sering diblokir
-select k.nama_kategori, count(bv.id_video) as jumlah_blokir
-from kutube_db.kategori k,kutube_db.blocked_video bv 
-inner join kutube_db.video_has_hastag_kategori vhhk
-on bv.id_video=vhhk.video_id_video
-group by k.nama_kategori;
 
-# i. Menampilkan top donatur dari setiap channel
-# j. buat 
+-- e. Tampilkan semua creator yang telah terverifikasi (memenuhi syarat untuk adsense)
+SELECT username, user_type, is_verified 
+FROM kutube_db.user 
+WHERE is_verified = 1 AND user_type = 'Creator';
+
+
+-- f. Tampilkan jumlah video iklan yang diperoleh oleh pengguna terverifikasi selama September–Oktober 2025
+SELECT U.id_user AS `ID User`, 
+       U.username AS Username, 
+       SUM(A.jumlah_iklan) AS `Jumlah Video Iklan` 
+FROM user U 
+LEFT JOIN video V ON U.id_user = V.id_uploader 
+LEFT JOIN adsense A ON A.id_video = V.id_video 
+WHERE U.is_verified = 1 
+  AND (YEAR(V.upload_time) = 2025 AND MONTH(V.upload_time) BETWEEN 9 AND 10) 
+GROUP BY `ID User`;
+
+
+-- g. Tampilkan 5 kategori video yang paling sering diblokir
+SELECT VHH.kategori_id_kategori AS Category, 
+       COUNT(BD.id_video) AS Blocked 
+FROM blocked_video BD 
+JOIN video V ON V.id_video = BD.id_video 
+JOIN video_has_hastag_kategori VHH ON V.id_video = VHH.video_id_video 
+GROUP BY Category 
+ORDER BY Blocked DESC 
+LIMIT 5;
+
+
+-- h. Tampilkan 5 donatur dengan jumlah donasi terbesar (Top 5 biggest donation)
+SELECT D.id_sender AS ID, 
+       U.username AS Username, 
+       D.jumlah_donasi AS `Jumlah Donasi` 
+FROM donation D 
+JOIN user U ON U.id_user = D.id_sender 
+ORDER BY D.jumlah_donasi DESC 
+LIMIT 5;
+
+
+-- i. Tampilkan seluruh pengguna yang memiliki akun premium
+SELECT * 
+FROM user U 
+WHERE U.is_prime = 1;
